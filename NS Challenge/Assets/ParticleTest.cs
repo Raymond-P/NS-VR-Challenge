1b85bd61565445679f6444663883b2daad1d6a01
Raymond-P <raymondperez158@gmail.com>
2018-09-05 01:23:25 -0400

    Intial commit

---
+++ NS Challenge/Assets/ParticleTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTest : MonoBehaviour {
+
+	public void RandomTeleport()
+    {
+        Vector3 cur_position = transform.position; 
+        gameObject.transform.position = new Vector3(cur_position.x, Random.Range(0, 4), cur_position.z);
+    }
+}
